72b50f76748ec60c56e3958843fba1adce87b314
hzx4585 <519907066@qq.com>
2017-03-28 12:09:36 +0800

    手机号阶段

--- OldFriend/SignUpTableViewController.swift
+++ OldFriend/SignUpTableViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class SignUpTableViewController: UITableViewController {
-    @IBOutlet weak var switchButton: UISwitch!
-
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBAction func getVerificationCode(_ sender: UIButton) {
+        if (checkMobilephoneNumber(phoneNumber: phoneNumberTextField.text!)) {
+            self.performSegue(withIdentifier: "getVerificaionCodeSegue", sender: self)
+        }
+    }
+    
+    private func checkMobilephoneNumber(phoneNumber:String) -> Bool{
+        let result = true
+        return result
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "getVerificaionCodeSegue" {
+            let destinationController = segue.destination as! InputVerificationCodeTableViewController
+            print("phoneNumber" + phoneNumberTextField.text!)
+            if let phoneNumber = phoneNumberTextField.text {
+                destinationController.messageTextField?.text = phoneNumber
+                //destinationController.setValue(phoneNumber, forKey: "phoneNumber")
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationController?.navigationBar.topItem?.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        return false
     }
     
     override func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
